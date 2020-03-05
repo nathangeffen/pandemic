8d966db8f04da31dd024ae11c6dc53ab8cd2ca71
Nathan Geffen <nathangeffen@gmail.com>
2020-03-05 07:45:26 +0200

    Improved styling of graph.

--- js/infectionchart.js
+++ js/infectionchart.js
@@ -43,6 +43,7 @@
 		        scales: {
                             xAxes: [{
                                 ticks: {
+                                    fontSize: 10,
                                     beginAtZero: true,
                                 },
                                 display: true,
@@ -53,12 +54,13 @@
                             }],
                             yAxes: [{
                                 ticks: {
+                                    fontSize: 10,
                                     beginAtZero: true,
                                     callback: function(value, index, values) {
-                                        if (value >= 1000000) {
-                                            return Math.round(value / 1000000)
-                                                + "M";
-                                        } else {
+                                        // if (value >= 1000000) {
+                                        //     return Math.round(value / 1000000)
+                                        //         + "M";
+                                        // } else {
                                             const s = new Intl.NumberFormat(
                                                 'en-US',
                                                 {
@@ -66,7 +68,7 @@
                                                     maximumFractionDigits: 0
                                                 }).format(value);
                                             return s;
-                                        }
+                                        // }
                                     }
                                 },
                                 display: true,
